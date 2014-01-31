3b6abeaee65c88b91443a75241f67fe160050b65
Ruben Elzerman <r.m.elzerman@gmail.com>
2014-01-31 11:30:54 +0100

    Yeoman test

---
+++ app/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Club Curated</title>
+</head>
+
+<body>
+Club Curated via Heroku.. using git...testing log on github....1
+</body>
+
+</html>
